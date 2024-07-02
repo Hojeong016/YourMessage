e30c40ab939debeaf404e77e5070522ebe4e00cf
Hojeong016 <149287600+Hojeong016@users.noreply.github.com>
2024-07-02 16:34:27 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,230 @@
+import os
+from openai import OpenAI
+import streamlit as st
+import pandas as pd
+from base64 import b64encode
+
+image_path_map = {
+    "매우 위험": "scoreImg/5.jpg",
+    "위험": "scoreImg/4.jpg",
+    "확인 필요": "scoreImg/3.jpg",
+    "안전": "scoreImg/2.jpg",
+    "매우 안전": "scoreImg/1.jpg"
+}
+
+# CSS 스타일 지정
+st.markdown("""
+<style>
+.section-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+}
+.section-description {
+    font-size: 12px;
+    color: #666666;
+    margin-bottom: 20px;
+}
+
+.section-line {
+    border-top: 1px solid #dddddd;
+    margin-bottom: 20px;
+}
+
+.responsive-table {
+    width: 100%;
+    max-width: 100%;
+    border-collapse: collapse;
+    overflow-x: auto;
+    display: block;
+}
+.responsive-table th, .responsive-table td {
+    padding: 8px;
+    text-align: left;
+    border: 1px solid #dddddd;
+    white-space: nowrap; 
+}
+.responsive-table th {
+    background-color: #f2f2f2;
+    font-weight: bold; 
+}
+.responsive-table td {
+    min-width: 200px; 
+}
+
+.footer {
+    display: flex;
+    justify-content: flex-end; 
+    align-items: center; 
+    padding: 10px; 
+    border-top: 1px solid #dddddd; 
+}
+
+.footer-logo {
+    width: 150px; 
+    height: auto; 
+    cursor: pointer; 
+    margin-right: 10px; /* 이미지와 만든이 텍스트 간 간격을 조정 */
+}
+
+.footer-small-text {
+    font-size: 10px; 
+    margin-right: 10px; 
+}
+
+.footer-light-text {
+    font-size: 10px; 
+    color: #666666; 
+}
+
+.reference-info {
+    font-size: 10px; 
+    color: #666666; 
+    margin-top: 5px; 
+}
+</style>
+""", unsafe_allow_html=True)
+
+os.environ["OPENAI_API_KEY"] = st.secrets['API_KEY']
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+st.markdown("<div class='section-title'>너의 문자는&#128233;</div>", unsafe_allow_html=True)
+
+st.markdown("<div class='section-line'></div>", unsafe_allow_html=True)
+
+# 섹션 설명 출력
+st.markdown("""
+<div class='section-description'>
+    비영리 공익광고 모임 '발광'에서 스미싱 문자 사기를 예방하기 위해 만든 '너의문자는'을 소개합니다.<br> 
+    금융감독원에 따르면, 스미싱 피해액의 93.9%가 50대 이상 연령층에서 발생하고 있다고 합니다.<br>  
+    이 서비스를 통해 부모님들이 안전하게 문자 서비스를 이용하실 수 있도록 도와드리고자 하니, <br> 
+    많이 사용해 주시기 바랍니다.
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("<div class='section-line'></div>", unsafe_allow_html=True)
+
+keyword = st.text_area("의심되는 문자를 입력 후 확인을 눌러주세요.")
+
+if st.button('확인:mag_right:'): 
+    with st.spinner('문자를 확인하고 있습니다'):
+        # 첫 번째 요청: 스팸 여부 확인 및 점수 반환
+            chat_completion1 = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": keyword,
+                    },
+                    {
+                        "role": "system",
+                        "content": (
+                            "이 문자가 스팸 또는 사기 문자인지 확인하고, 0에서 100 사이의 숫자로 사기 위험도를 점수로 알려줘.점수가 낮을수록(0에 가까울수록) 사기 위험이 크고,높을수록(100에 가까울수록) 안전해. 이때 분석 내용은 알려주지말고 점수만 알려줘 0~100까지 숫자로만 "
+                    
+                        ),
+                    }
+                ],
+                model="gpt-4o",
+            )
+
+        # 첫 번째 요청의 응답에서 점수 추출
+            score = int(chat_completion1.choices[0].message.content.strip())
+        
+        # 점수를 기준으로 response 결정
+            if 0 <= score <= 20:
+                response = "매우 위험"
+            elif 21 <= score <= 40:
+                response = "위험"
+            elif 41 <= score <= 60:
+                response = "확인 필요"
+            elif 61 <= score <= 80:
+                response = "안전"
+            else:
+                response = "매우 안전"
+
+            # 결과 출력
+            # st.write(f"점수 판단 결과: {response}")
+            
+              # response에 따른 이미지 추가
+            if response in image_path_map:
+                image_path = image_path_map[response]
+                st.image(image_path, caption=response, width=150)
+        
+        
+        # 두 번째 요청: 점수 판단 근거 설명 요청
+            chat_completion2 = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": keyword,
+                    },
+                    {
+                        "role": "system",
+                        "content": (
+                            "문자를 객관적으로 분석하여 아래의 일곱가지 기준으로 설명해줘"
+                            "발신자 정보 : (예시 <국제 발신>인데 국내 기업의 내용을 담고 있는 경우, <웹 발신>인 경우에는 어떤 국가에서 발신하고 있는지 요약해줘 또한 발신자 정보에 대해 더 자세히 언급해줘)"
+                            "의심스러운 링크 : (예시 링크의 철자에 오류 유무와 클릭 유도가 없는지 분석해줘)" 
+                            "긴급성 강조 :"
+                            "금전적 요구 :"
+                            "철자 오류 :"
+                            "의심 키워드 : (스팸의심 키워드가 있는지 파악해줘)"
+                            "이 외의 사유 :"
+                            "각 기준에 대해 세 줄 이내로 친절하고 쉽게 설명해줘 존댓말을 사용했으면 좋겠어. 목록화된 결과로 번호없이 글자와 : 만 출력해줘."
+                        ),
+                    }
+                ],
+                model="gpt-4o",
+            )
+        
+        # 두 번째 요청의 응답
+            result2 = chat_completion2.choices[0].message.content
+            
+        # 응답을 딕셔너리로 변환 (예시로 콜론을 기준으로 분리)
+            ai_explanation = dict(item.split(": ") for item in result2.split("\n") if ": " in item)
+
+        # 딕셔너리를 데이터프레임으로 변환
+            df = pd.DataFrame(list(ai_explanation.items()), columns=["기준", "설명"])
+            
+        # 테이블을 HTML로 변환
+            table_html = df.to_html(index=False, escape=False)
+
+        # 테이블 이름 지정
+            st.write("<strong><판단 근거 설명></strong>",  unsafe_allow_html=True)
+
+        # 테이블 표시
+            st.markdown(f'<div class="responsive-table">{table_html}</div>', unsafe_allow_html=True)
+            
+            st.markdown("<div class='reference-info'><strong> &#128680; AI를 활용하여 최대한 많은 테스트를 진행하였습니다. <br> 다만 100% 정확하진 않을수 있으니 의심이 가는 문자는 주의하세요.</strong></div>", unsafe_allow_html=True)
+            st.markdown("<br><br>", unsafe_allow_html=True)     
+
+# 참조 정보 출력
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<div class='footer'>", unsafe_allow_html=True)
+
+# 이미지를 클릭했을 때 이동할 링크 URL
+link_url = "https://www.instagram.com/valgwang/"
+
+# 이미지 파일 읽기
+with open('logoImg/logo.png', 'rb') as f:
+    image_data = f.read()
+
+# 이미지 데이터를 base64로 인코딩
+image_base64 = b64encode(image_data).decode()
+
+# 이미지를 클릭 시 이동할 JavaScript 코드 생성
+js_code = f'''
+<script>
+function goToLink() {{
+    window.open("{link_url}", "_blank");
+}}
+</script>
+'''
+
+# JavaScript 코드 삽입 (unsafe_allow_html 사용)
+st.markdown(js_code, unsafe_allow_html=True)
+
+# 이미지에 onClick 이벤트 추가하여 링크로 이동
+st.markdown(f"<img src='data:image/png;base64,{image_base64}' class='footer-logo' onclick='goToLink()' style='cursor: pointer; width: 150px;'></p>", unsafe_allow_html=True)
+
+st.markdown("<div  class='footer-small-text'>도움 주신 분: 채호정</div>", unsafe_allow_html=True)
+st.markdown("<div  class='footer-light-text'>&#128231;coghwjd4051@gmail.com</div>", unsafe_allow_html=True)
+st.markdown("</div>", unsafe_allow_html=True)
